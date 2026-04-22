62fe0373106f4e7a56f0811c77f79de3b6ec00ec
brunofranciscojs <ednamaral39@yahoo.com.br>
2026-04-22 11:47:23 -0300

    fix sitemap

--- app/sitemap.js
+++ app/sitemap.js
@@ -2,10 +2,17 @@ import { akumasnomi } from '../lib/data';
 
 export default function sitemap() {
     const baseUrl = 'https://devilfruitencyclopedia.vercel.app';
-
+    const slugify = (text) => {
+        return text.toLowerCase()
+            .replace(/\s+/g, '-')           // Replace spaces with -
+            .replace(/[^a-z0-9-]/g, '')     // Remove all non-word chars
+            .replace(/-+/g, '-')           // Replace multiple - with single -
+            .replace(/^-+/, '')             // Trim - from start of text
+            .replace(/-+$/, '');            // Trim - from end of text
+    }
     // Get all fruit routes
     const fruitUrls = akumasnomi.map((fruta) => ({
-        url: `${baseUrl}/fruit/${fruta.id}`,
+        url: `${baseUrl}/fruit/${slugify(fruta.name)}`,
         lastModified: new Date(),
         changeFrequency: 'weekly',
         priority: 0.8,
